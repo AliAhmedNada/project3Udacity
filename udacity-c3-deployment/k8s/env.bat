074324933d9b595a3485d9bf0837b51f05642e67
AliAhmedNada <aliahmedalihassan@gmail.com>
2020-06-13 12:20:11 +0200

    adding apps/v1

---
+++ udacity-c3-deployment/k8s/env.bat
@@ -0,0 +1,8 @@
+set POSTGRESS_USERNAME=postgres
+set POSTGRESS_PASSWORD=postgres
+set POSTGRESS_DB=postgres
+set POSTGRESS_HOST=postgres.czpmigdfuga0.us-east-1.rds.amazonaws.com
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set AWS_BUCKET=awsbucket2020ooo
+set JWT_SECRET=helloworld
